ce7c4ed52cca265de3e70c227a73c27e23255282
kosmonamu <kosmonamu@gmail.com>
2023-08-27 12:46:51 +0900

    ascii

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s = "123";
+    s[0]++;
+
+    cout << s[0] << "\n";
+
+    char a = 'a';
+    cout << (int)a << "\n";
+
+    return 0;
+}
