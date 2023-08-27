1658aa591159fc6bb579341bff3b161bd6c8387b
kosmonamu <kosmonamu@gmail.com>
2023-08-27 11:42:10 +0900

    bufferflush

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+// cin -- space, enter까지만
+// >> getline
+#include <iostream>
+
+using namespace std;
+
+int T;
+string s;
+
+int main(){
+    
+    cin >> T;
+    string bufferflush;
+    getline(cin, bufferflush);
+
+    for(int i=0; i<T; i++){
+        getline(cin, s);
+        cout << s << "\n";
+    }
+
+    return 0;
+}
